21c26df30903a83e488ec369324c4940fcd89b0e
Hope Elizabeth <hopeyhope@me.com>
2016-03-30 11:55:26 -0700

    First commit

---
+++ On The Map/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  On The Map
+//
+//  Created by Hope on 3/30/16.
+//  Copyright © 2016 Hope Elizabeth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
