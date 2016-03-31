baf74d19e8f7ab8e3aa14e4b300ff3359c7ce334
Hope Elizabeth <hopeyhope@me.com>
2016-03-30 20:41:18 -0700

    Set up the basic controllers on storyboard.

---
+++ InformationPostingViewController.swift
@@ -0,0 +1,24 @@
+//
+//  InformationPostingViewController.swift
+//  On The Map
+//
+//  Created by Hope on 3/30/16.
+//  Copyright © 2016 Hope Elizabeth. All rights reserved.
+//
+
+import UIKit
+
+class InformationPostingViewController: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
--- MapAndTableTabbedViewController.swift
+++ MapAndTableTabbedViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MapAndTableTabbedViewController.swift.swift
 //  On The Map
 //
 //  Created by Hope on 3/30/16.
@@ -8,18 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class MapAndTableTabbedViewController: UITableViewController
+{
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
+    
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
